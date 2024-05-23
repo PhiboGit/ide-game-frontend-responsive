1c8d40565e29b85d6bf905dcdaae2d5e5c57af1a
Phibo <phibo96@gmail.com>
2024-05-23 17:43:01 +0200

    added comments

--- src/gamepage/game/stateManagement/createPartialContextStore.tsx
+++ src/gamepage/game/stateManagement/createPartialContextStore.tsx
@@ -2,12 +2,23 @@ import React, { createContext, useCallback, useContext, useRef, useSyncExternalS
 
 export default function createPartialContextStore<T>(initialData: T){
 
-  //utility function to merge partital objects deeply
+  /**
+   * Merges two objects into one.
+   * It only replaces the objects of the target that are specified in the source.
+   * 
+   * For use in a hook, it only notifies objects that are changed. 
+   * Non source objects are not notified as the target is kept immutable.
+   * Only the source objects of taget are mutated.
+   * 
+   * @param target object to manipulate
+   * @param source Partial objects of target to merge into target
+   * @returns the same target with updated source objects
+   */
   function mergeDeep<T>(target: T, source: DeepPartial<T>): T {
     // Make sure it is a record object with a key: value.
     // Have to execlude arrays as they are objects and
     // and we cannot otherwise observe the changes to the array itself, eg. adding or removing items.
-    // just reasign the array as a new array/ or spread, like normaly to get a state update
+    // just reasign the array as a new array/ or spread, like normaly to get a state update!
     function isObject(obj: any): obj is Record<string, any>{
       return obj && typeof obj === 'object' && !Array.isArray(obj);
     } 
@@ -24,7 +35,8 @@ export default function createPartialContextStore<T>(initialData: T){
   }
 
   /**  
-   * Recursive Partial Type
+   * Recursive Partial Type. Makes all properties P of T optional(P?).
+   * And T[P] also optional if it is an object, else it keeps it type.
    * Patial<T> = { [P in keyof T]?: T[P] | undefined } as defined by TypeScript.
   */
   type DeepPartial<T> = {
@@ -34,9 +46,11 @@ export default function createPartialContextStore<T>(initialData: T){
       : T[P];
   };
 
+  type DeepPartialUpdater = (data: T) => DeepPartial<T>
+
   function useStoreData() :{
     get: () => T,
-    set: (updater: (data: T) => DeepPartial<T>) => void,
+    set: (updater: DeepPartialUpdater) => void,
     subscribe: (callback: () => void) => () => void
   } {
 
@@ -44,9 +58,21 @@ export default function createPartialContextStore<T>(initialData: T){
   
     const get = useCallback(() => store.current,[])
   
-    const set = (updater: (data: T) => DeepPartial<T>) => {
-      // TODO: nested objects
-      store.current = mergeDeep({...store.current},updater(store.current))
+    
+  /**
+   * Updates the store with the provided updater function and notifies all subscribers.
+   * 
+   * The updater function can return a partial nested update.
+   * Only the objects specified in the update will be updated and subscribers will be notified if they have selected the same object.
+   * objects not specifed by the updater are not mutated and subscribers are not notified!
+   * 
+   * @param updater - A function that takes the current store state and returns a partial update.
+   * @return 
+   */
+    const set = (updater: DeepPartialUpdater) => {
+      // i dont know: mergeDeep({...store.current} ,updater(store.current))
+      // might not be nessary to copy the store.current object. mergeDeep copies all the objects.
+      store.current = mergeDeep(store.current ,updater(store.current))
       subscribers.current.forEach((callback) => callback());
     }
 
@@ -66,10 +92,17 @@ export default function createPartialContextStore<T>(initialData: T){
     return <Context.Provider value={useStoreData()}>{children}</Context.Provider>
   }
 
-  
+  /**
+   * If you select the root, you are not notifed of any nested changes within the store, only if you replace the root object!
+   * To notify all subscribers with nested selectors you need to do a deep copy of the root object and pass it to the selector.
+   * 
+   * 
+   * @param selector a function that returns a (nested) state
+   * @returns Returns the selected state (nested) and a function to update the state root object
+   */
   function useStore<SelectorOutput>(
     selector: (store: T) => SelectorOutput
-  ): [SelectorOutput, (updater: (data: T) => DeepPartial<T>) => void]   {
+  ): [SelectorOutput, (updater: DeepPartialUpdater) => void]   {
     const context = useContext(Context)
     if (!context) {
       throw new Error('context not found! Must be within a provider.')
