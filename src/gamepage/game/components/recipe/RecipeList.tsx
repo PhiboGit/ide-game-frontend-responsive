e0032a70562f1afa625582a3c88f0f567538c85d
phibo <phibo96@gmail.com>
2024-06-17 13:52:09 +0200

    extracted recipeList

---
+++ src/gamepage/game/components/recipe/RecipeList.tsx
@@ -0,0 +1,158 @@
+import { Box, Collapse, Divider, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Typography } from "@mui/material";
+import { ProfessionId } from "../../gameTypes";
+import useGameDataState from "../../stateManagement/GameData/useGameData"
+
+import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
+import React from "react";
+import ResourceTile from "../../components/tiles/ResourceTile";
+import { getProfessionRecipes } from "./getProfessionRecipes";
+
+
+type RecipeListProps = {
+    profession: ProfessionId
+    onChange: (recipeType: 'resource' | 'rarityResource' | 'item' , recipeId: string) => void
+}
+export default function RecipeList({profession, onChange}: RecipeListProps) {
+  const gameData = useGameDataState((data) => data)
+
+  const {resourceRecipes, rarityResourceRecipes, itemRecipes} = getProfessionRecipes(profession, gameData);
+
+  const [openResources, setOpenResources] = React.useState(true);
+  const [openRarityResources, setOpenRarityResources] = React.useState(true);
+  const [openItems, setOpenItems] = React.useState(true);
+
+
+  const [selected, setSelected] = React.useState<string>('');
+  const handleSelect = (recipeType: 'resource' | 'rarityResource' | 'item', recipeId: string) => {
+    setSelected(recipeId);
+    onChange(recipeType, recipeId);
+  }
+
+  return (
+    <Box display='flex'>
+      <List
+        dense
+        sx={{
+          width: 150,
+          position: 'relative',
+          overflow: 'hidden',
+          
+        }}
+        subheader={<li />}
+      >
+      {resourceRecipes.size > 0 && <ListItemButton onClick={() => setOpenResources(!openResources)} sx={{ padding: 0 }}>
+        {openResources ? <ExpandLess fontSize="small"/> : <ExpandMore fontSize="small"/>}
+        <ListItemText secondary="Resources" />
+      </ListItemButton>}
+      {resourceRecipes.size > 0 && <Collapse in={openResources} timeout="auto" unmountOnExit>
+        <List dense component="div" disablePadding>
+          {Array.from(resourceRecipes.entries()).map(([recipeId, recipe]) => (
+             <ListItem
+              key={recipeId}
+              secondaryAction={
+                <ResourceTile size={1.3} elevation={0} resourceId={recipe.resource} />
+              }
+              // no padding to be compact
+              disablePadding
+              sx={{ 
+                // padding left, to align with the the headline + icon
+                pl: 2.5,
+                // overwrite to have the secendaryAction (icon) on the right
+                '& .MuiListItemSecondaryAction-root': {
+                  right: 4
+                },
+                // no padding bewteen the secondaryAction and the text of the ListItemButton
+                '& .MuiListItemButton-root': {
+                  padding: 0
+                }
+               }}
+            >
+              <ListItemButton 
+                selected={selected === recipeId}
+                onClick={() => handleSelect("resource", recipeId)} 
+                // no padding to be compact
+                sx={{ padding: 0 }}
+                >
+                <ListItemText 
+                    primary={recipe.displayName} 
+                    // with the paddingRight and noWrap the text is never under the secondaryAction icon!
+                    primaryTypographyProps={{ noWrap: true }} 
+                    sx={{ paddingRight: 3.5 }}/>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>}
+
+      {rarityResourceRecipes.size > 0 &&<ListItemButton onClick={() => setOpenRarityResources(!openRarityResources)} sx={{ padding: 0 }}>
+        {openRarityResources ? <ExpandLess fontSize="small"/> : <ExpandMore fontSize="small"/>}
+        <ListItemText secondary="Quality Resources" />
+      </ListItemButton>}
+      {rarityResourceRecipes.size > 0 &&<Collapse in={openRarityResources} timeout="auto" unmountOnExit>
+        <List dense component="div" disablePadding>
+          {Array.from(rarityResourceRecipes.entries()).map(([recipeId, recipe]) => (
+             <ListItem
+              key={recipeId}
+              secondaryAction={
+                <ResourceTile size={1.3} elevation={0} resourceId={recipe.resource_rarity.common!} />
+              }
+              disablePadding
+              sx={{ 
+                pl: 2.5,
+                '& .MuiListItemSecondaryAction-root': {
+                  right: 4
+                },
+                '& .MuiListItemButton-root': {
+                  padding: 0
+                }
+               }}
+            >
+              <ListItemButton 
+                selected={selected === recipeId}
+                onClick={() => handleSelect("rarityResource", recipeId)} 
+                sx={{ padding: 0 }}
+              >
+                <ListItemText primary={recipe.displayName} primaryTypographyProps={{ noWrap: true }} sx={{ paddingRight: 3.5 }}/>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>}
+
+      {itemRecipes.size > 0 &&<ListItemButton onClick={() => setOpenItems(!openItems)} sx={{ padding: 0 }}>
+        {openItems ? <ExpandLess fontSize="small"/> : <ExpandMore fontSize="small"/>}
+        <ListItemText secondary="Items" />
+      </ListItemButton>}
+      {itemRecipes.size > 0 &&<Collapse in={openItems} timeout="auto" unmountOnExit>
+        <List dense component="div" disablePadding>
+          {Array.from(itemRecipes.entries()).map(([recipeId, recipe]) => (
+             <ListItem
+              key={recipeId}
+              secondaryAction={
+                <></>
+              }
+              disablePadding
+              sx={{ 
+                pl: 2.5,
+                '& .MuiListItemSecondaryAction-root': {
+                  right: 4
+                },
+                '& .MuiListItemButton-root': {
+                  padding: 0
+                }
+               }}
+            >
+              <ListItemButton 
+                selected={selected === recipeId}
+                onClick={() => handleSelect("item", recipeId)} sx={{ padding: 0 }}>
+                <ListItemText primary={recipe.displayName} primaryTypographyProps={{ noWrap: true }} sx={{ paddingRight: 3.5 }}/>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Collapse>}
+    </List>
+    <Divider orientation="vertical" flexItem/>
+    </Box>
+  )
+}
--- src/gamepage/game/sections/refining/SmeltingSection.tsx
+++ src/gamepage/game/sections/refining/SmeltingSection.tsx
@@ -1,136 +1,34 @@
 import { Box, Collapse, Divider, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, ListSubheader, Typography } from "@mui/material";
 import { ProfessionId } from "../../gameTypes";
 import useGameDataState from "../../stateManagement/GameData/useGameData"
-import { getProfessionRecipes } from "./getProfessionRecipes";
+import { getProfessionRecipes } from "../../components/recipe/getProfessionRecipes";
 import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
 import SendIcon from '@mui/icons-material/Send';
 import React from "react";
 import ResourceTile from "../../components/tiles/ResourceTile";
+import RecipeList from "../../components/recipe/RecipeList";
 
-
+type SelectedRecipe = {
+  recipeType: 'resource' | 'rarityResource' | 'item' | null,
+  recipeId: string | null
+}
 export default function SmeltingSection() {
-  const gameData = useGameDataState((data) => data)
-
-  const {resourceRecipes, rarityResourceRecipes, itemRecipes} = getProfessionRecipes('smelting', gameData);
-
-  const [openResources, setOpenResources] = React.useState(true);
-  const [openRarityResources, setOpenRarityResources] = React.useState(true);
-  const [openItems, setOpenItems] = React.useState(true);
-
-
-  const [selected, setSelected] = React.useState<string>('');
-  const handleSelect = (recipeId: string) => {
-    setSelected(recipeId);
+  const [selected, setSelected] = React.useState<SelectedRecipe>({recipeType: null, recipeId: null});
+  
+  const handleSelect = (recipeType: 'resource' | 'rarityResource' | 'item', recipeId: string) => {
+    setSelected({recipeType, recipeId});
   }
 
   return (
     <Box display='flex'>
-      <List
-        dense
-        sx={{
-          width: '100%',
-          maxWidth: 200,
-          position: 'relative',
-          overflow: 'hidden',
-          
-        }}
-        subheader={<li />}
-      >
-      <ListItemButton onClick={() => setOpenResources(!openResources)} sx={{ gap: 0.0, padding: 0 }}>
-        {openResources ? <ExpandLess fontSize="small"/> : <ExpandMore fontSize="small"/>}
-        <ListItemText secondary="Resources" />
-      </ListItemButton>
-      <Collapse in={openResources} timeout="auto" unmountOnExit>
-        <List dense component="div" disablePadding>
-          {Array.from(resourceRecipes.entries()).map(([recipeId, recipe]) => (
-             <ListItem
-              key={recipeId}
-              secondaryAction={
-                <ResourceTile size={1.3} elevation={0} resourceId={recipe.resource} />
-              }
-              disablePadding
-              sx={{ 
-                pl: 2.5,
-                '& .MuiListItemSecondaryAction-root': {
-                  right: 4
-                }
-               }}
-            >
-              <ListItemButton 
-                selected={selected === recipeId}
-                onClick={() => handleSelect(recipeId)} sx={{ padding: 0 }}>
-                <ListItemText primary={recipe.displayName} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Collapse>
-
-      <ListItemButton onClick={() => setOpenRarityResources(!openRarityResources)} sx={{ gap: 0.0, padding: 0 }}>
-        {openRarityResources ? <ExpandLess fontSize="small"/> : <ExpandMore fontSize="small"/>}
-        <ListItemText secondary="Quality Resources" />
-      </ListItemButton>
-      <Collapse in={openRarityResources} timeout="auto" unmountOnExit>
-        <List dense component="div" disablePadding>
-          {Array.from(rarityResourceRecipes.entries()).map(([recipeId, recipe]) => (
-             <ListItem
-              key={recipeId}
-              secondaryAction={
-                <ResourceTile size={1.3} elevation={0} resourceId={recipe.resource_rarity.common!} />
-              }
-              disablePadding
-              sx={{ 
-                pl: 2.5,
-                '& .MuiListItemSecondaryAction-root': {
-                  right: 4
-                }
-               }}
-            >
-              <ListItemButton 
-                selected={selected === recipeId}
-                onClick={() => handleSelect(recipeId)} 
-                sx={{ padding: 0 }}
-              >
-                <ListItemText primary={recipe.displayName} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Collapse>
-
-      <ListItemButton onClick={() => setOpenItems(!openItems)} sx={{ gap: 0.0, padding: 0 }}>
-        {openItems ? <ExpandLess fontSize="small"/> : <ExpandMore fontSize="small"/>}
-        <ListItemText secondary="Items" />
-      </ListItemButton>
-      <Collapse in={openItems} timeout="auto" unmountOnExit>
-        <List dense component="div" disablePadding>
-          {Array.from(itemRecipes.entries()).map(([recipeId, recipe]) => (
-             <ListItem
-              key={recipeId}
-              secondaryAction={
-                <></>
-              }
-              disablePadding
-              sx={{ 
-                pl: 2.5,
-                '& .MuiListItemSecondaryAction-root': {
-                  right: 4
-                }
-               }}
-            >
-              <ListItemButton 
-                selected={selected === recipeId}
-                onClick={() => handleSelect(recipeId)} sx={{ padding: 0 }}>
-                <ListItemText primary={recipe.displayName} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Collapse>
-    </List>
-    <Divider orientation="vertical" flexItem/>
+      <RecipeList profession='smelting' onChange={handleSelect}/>
+      <Box>
+        {(selected.recipeType === 'resource' && selected.recipeId)  && 
+          <Typography variant="h6">Recipe: {selected.recipeId}</Typography>
+        }
+      </Box>
     </Box>
   )
 }
