90791089bbb7b8f21cd92362e3c12d2cac01bb22
phibo <phibo96@gmail.com>
2024-06-21 15:19:42 +0200

    moved text as a child component

--- src/gamepage/game/components/tiles/ResourceTile.tsx
+++ src/gamepage/game/components/tiles/ResourceTile.tsx
@@ -9,12 +9,12 @@ import { ResourceId } from "../../gameTypes";
 interface ResourceTileProps {
   onClick?: (event: React.MouseEvent<HTMLInputElement>) => void
   resourceId: ResourceId
-  count?: number
   size? : number
   elevation?: number
+  children?: React.ReactNode
 }
 
-export default function ResourceTile({ onClick, resourceId, count = 0, size = 3.5, elevation = 1}: ResourceTileProps) {
+export default function ResourceTile({ onClick, resourceId, size = 3.5, elevation = 1, children}: ResourceTileProps) {
   const resource = useGameData((data) => data.resourceData[resourceId])
   if (!resource) {
     console.log(`Resource ${resourceId} not found`, resource)
@@ -35,15 +35,7 @@ export default function ResourceTile({ onClick, resourceId, count = 0, size = 3.
       tooltipComponent={<ResourceTooltip resourceId={resourceId}/>}
       rarityBorderColor={resource.rarity}
     >
-      <Typography 
-        lineHeight={.95}
-        fontSize={'.75rem'}
-        variant="body2"
-        textAlign='end' 
-        sx={{ position: 'absolute', bottom: 0, left: 0, width: '100%', padding: '1.5px 3.5px', textShadow: '1px 1px 1px black'}}
-      >
-        {count ? count : ''}
-      </Typography>
+      {children}
     </BaseTile>
   );
 }
--- src/gamepage/game/sections/character/inventory/InventoryResourceGridItem.tsx
+++ src/gamepage/game/sections/character/inventory/InventoryResourceGridItem.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 import ResourceTile from "../../../components/tiles/ResourceTile";
 import ClickAwayPopper from "../../../components/common/ClickAwayPopper";
@@ -8,7 +8,7 @@ import { ResourceId } from "../../../gameTypes";
 
 export default function InventoryResourceGridItem({ resourceId }: { resourceId: ResourceId }) {
 
-  const value = useCharacter((char) => char.resources[resourceId])
+  const units = useCharacter((char) => char.resources[resourceId])
 
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
 
@@ -23,12 +23,21 @@ export default function InventoryResourceGridItem({ resourceId }: { resourceId: 
   };
 
   return (
-    value > 0 && <Grid item key={resourceId} >
+    units > 0 && <Grid item key={resourceId} >
       <ResourceTile 
         resourceId={resourceId} 
-        count={value} 
         onClick={openPopperResource}
-      />
+      >
+        <Typography 
+        lineHeight={.95}
+        fontSize={'.75rem'}
+        variant="body2"
+        textAlign='end' 
+        sx={{ position: 'absolute', bottom: 0, left: 0, width: '100%', padding: '1.5px 3.5px', textShadow: '1px 1px 1px black'}}
+      >
+        {units ? units : ''}
+      </Typography>
+      </ResourceTile>
       <ClickAwayPopper anchorEl={anchorEl} setAnchorEl={setAnchorEl}>
         <ResourceMenu resourceId={resourceId} closeMenu={closePopper}/>
       </ClickAwayPopper>
