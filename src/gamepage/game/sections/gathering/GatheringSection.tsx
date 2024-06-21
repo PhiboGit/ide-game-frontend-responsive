3700c77dcf8d57d4b1564165d6c53aa9b996e7b2
phibo <phibo96@gmail.com>
2024-06-21 21:24:44 +0200

    center nodes

--- src/gamepage/game/sections/gathering/GatheringSection.tsx
+++ src/gamepage/game/sections/gathering/GatheringSection.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import GatheringTile from '../../components/tiles/GatheringNodeTile'
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Container, Grid, Typography } from '@mui/material'
 import StartActionController from '../../components/actions/StartActionController';
 import useGameDataState from '../../stateManagement/GameData/useGameData';
 import { GatheringMsg, ProfessionId } from '../../gameTypes';
@@ -56,33 +56,35 @@ function GatheringSection({profession }: {profession: ProfessionId}) {
     websocketService.send(msg);
   };  
 
-  return (      
-    <Box display="flex" flexDirection="column" gap="1rem" alignItems='center' padding='0.5rem 1rem 1rem 1rem'>
-      <Box width='200px' display='flex' flexDirection={'column'} alignSelf={'center'} alignItems='center'>
-        <Typography variant="h5" noWrap textTransform={'capitalize'}>{profession} Lv. {level}</Typography>
-        <LevelProgressBar progress={progress} levelUpInExp={levelUpInExp} />
-      </Box>
-      <Grid container spacing={2} >
-        {nodes.map(nodeId => 
-          <Grid item key={nodeId} xs='auto'>
-            <GatheringTile 
-              size={8} 
-              nodeId={nodeId}
-              onClick={() => setSelectedAction(nodeId)}
-              selected={selectedAction === nodeId}
-              />
-          </Grid>
-        )}
-      </Grid>
-      <StartActionController 
-        limit={limit}
-        setLimit={setLimit}
-        iterations={iterations}
-        setIterations={setIterations}
-        startDisabled={selectedAction === null}
-        onClickStart={handleStartClick}
-      />     
-    </Box>  
+  return (   
+    
+      <Box display="flex" flexDirection="column" gap="1rem" alignItems='center' padding='0.5rem 1rem 1rem 1rem'>
+        <Box width='200px' display='flex' flexDirection={'column'} alignSelf={'center'} alignItems='center'>
+          <Typography variant="h5" noWrap textTransform={'capitalize'}>{profession} Lv. {level}</Typography>
+          <LevelProgressBar progress={progress} levelUpInExp={levelUpInExp} />
+        </Box>
+        <Grid container spacing={2} justifyContent='center'>
+          {nodes.map(nodeId => 
+            <Grid item key={nodeId} xs='auto'>
+              <GatheringTile 
+                size={8} 
+                nodeId={nodeId}
+                onClick={() => setSelectedAction(nodeId)}
+                selected={selectedAction === nodeId}
+                />
+            </Grid>
+          )}
+        </Grid>
+        <StartActionController 
+          limit={limit}
+          setLimit={setLimit}
+          iterations={iterations}
+          setIterations={setIterations}
+          startDisabled={selectedAction === null}
+          onClickStart={handleStartClick}
+          />     
+      </Box>  
+   
   )
 
 }
