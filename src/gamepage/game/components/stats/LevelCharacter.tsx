2801f244966073a65a4b07134ce4e1fdc6e770eb
phibo <phibo96@gmail.com>
2024-06-21 13:41:08 +0200

    show level

--- src/gamepage/game/components/stats/LevelCharacter.tsx
+++ src/gamepage/game/components/stats/LevelCharacter.tsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import useCharacterState from "../../stateManagement/CharacterData/useCharacterData";
+import { getLevel } from "../../gameUtils";
 
 export default function LevelCharacter() {
-  const level = 1
-
+  const expChar = useCharacterState(char => char.expChar)
+  const level = getLevel(expChar)
   return (
     <Typography variant="body2">Level: {level}</Typography>
   )
